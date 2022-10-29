454d9524b4ed79e291d663beb82458044beadfac
Kharisma Tara Mukti <kharismataram@gmail.com>
2022-10-29 11:04:25 +0700

    fist

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php 
+//cara mencetak tulisan pada php
+ echo "hello word "; 
+
+//membuat variabel pada php
+ $nama= "Kharisma Tara Mukti ";
+ $program = "MSIB SEAL ";
+ $usia = 20 ;
+
+//gabungan mencetak dengan variabe; 
+ echo "Selamat Datang $nama, sekarang mengikuti program $program, berusia $usia"
+
+?>
